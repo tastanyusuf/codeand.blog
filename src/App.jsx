2515c48851fe4387812f6841cee2b2702f3e81cb
Yusuf <tastan.yusuf@proton.me>
2025-07-13 17:22:53 +0200

    V1

--- src/App.jsx
+++ src/App.jsx
@@ -16,20 +16,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Code and Blog</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+         Contact: <a href="mailto:support@codeand.blog">support@codeand.blog</a>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
 export default App
+
